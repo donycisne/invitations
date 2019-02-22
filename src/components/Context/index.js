9b464c781bb5c9982e052764309b06c288d06abe
Dony Cisneros <dony.cisne@gmail.com>
2019-02-22 03:16:37 -0300

    Refactor the Provider

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -1,6 +1,131 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const InvitationsContext = React.createContext();
 
-export const Provider = InvitationsContext.Provider;
+export class Provider extends Component {
+
+  state = {
+    isFiltered: false,
+    pendingGuest: "",
+    guests: []
+  };
+
+  lastGuestId = 0;
+
+  newGuestId = () => {
+    const id = this.lastGuestId;
+    this.lastGuestId += 1;
+    return id;
+  }
+
+  toggleGuestProperty = (property, id) => {
+    this.setState( prevState => ({
+      guests: prevState.guests.map(guest => {
+        if (id === guest.id) {
+          return {
+            ...guest,
+            [property]: !guest[property]
+          };
+        }
+        return guest;
+      })
+    }));
+  }
+
+  toggleConfirmation = (id) => {
+    this.toggleGuestProperty("isConfirmed", id);
+  }
+
+  toggleEditing = (id) => {
+    this.toggleGuestProperty("isEditing", id);
+  }
+
+  removeGuest = (id) => {
+    this.setState( prevState => ({
+      guests: prevState.guests.filter(
+        guest => id !== guest.id
+      )
+    }));
+  }
+
+  setName = (name, id) => {
+    this.setState( prevState => ({
+      guests: prevState.guests.map(guest => {
+        if(id === guest.id) {
+          return {
+            ...guest,
+            name
+          };
+        }
+        return guest;
+      })
+    }));
+  }
+
+  toggleFilter = () => {
+    this.setState( prevState => ({
+      isFiltered: !prevState.isFiltered
+    }));
+  }
+
+  handleNameInput = (e) => {
+    this.setState({
+      pendingGuest: e.target.value
+    });
+  }
+
+  newGuestSubmitHandler = (e) => {
+    e.preventDefault();
+    const id = this.newGuestId();
+    if (this.state.pendingGuest.trim() !== '') {
+      this.setState( prevState => ({
+        guests: [
+          {
+            name: prevState.pendingGuest,
+            isConfirmed: false,
+            isEditing: false,
+            id
+          },
+          ...prevState.guests
+        ],
+        pendingGuest: ''
+      }));
+    }
+  }
+
+  getAttendingGuests = () =>
+    this.state.guests.reduce(
+      (total, guest) => guest.isConfirmed ? total + 1 : total,
+    0);
+
+  getTotalInvited = () => this.state.guests.length;
+
+  numberUnconfirmed = () =>
+    this.getTotalInvited() - this.getAttendingGuests()
+
+  render() {
+    return (
+      <InvitationsContext.Provider value={{
+        actions: {
+          newGuestSubmitHandler:this.newGuestSubmitHandler,
+          pendingGuest: this.state.pendingGuest,
+          handleNameInput: this.handleNameInput,
+          toggleFilter: this.toggleFilter,
+          isFiltered: this.state.isFiltered,
+          totalInvited: this.getTotalInvited(),
+          numberAttending: this.getAttendingGuests(),
+          numberUnconfirmed: this.numberUnconfirmed(),
+          guests: this.state.guests,
+          toggleConfirmation: this.toggleConfirmation,
+          toggleEditing: this.toggleEditing,
+          setName: this.setName,
+          removeGuest: this.removeGuest,
+        }
+      }}>
+        {this.props.children}
+      </InvitationsContext.Provider>
+    );
+  }
+}
+
 export const Consumer = InvitationsContext.Consumer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "./components/Context";
 import './css/index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.register();
